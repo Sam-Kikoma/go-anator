9dad2128266f79122ae47accedbf37768ff9a7e7
Sam Kikoma <kikomasamson@gmail.com>
2026-05-24 00:15:54 +0200

    Refactor main function to accept book path as a command-line argument and display common books

--- bookworms/main.go
+++ bookworms/main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
 func main() {
-	bookworms, err := loadBookworms("testdata/bookworms.json")
+	var bookPath string
+	flag.StringVar(&bookPath,"path","","Enter a valid path eg <path>/<file.json>")
+	flag.Parse()
+
+	bookworms, err := loadBookworms(bookPath)
 	if err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "failed to load bookworms: %s\n", err)
 		os.Exit(1)
 	}
 
-	fmt.Println(bookworms)
+	commonBooks := findCommonBooks(bookworms)
+
+	fmt.Println("Here are the books in common:")
+	displayBooks(commonBooks)
 }
+
+func displayBooks(books []Book){
+	for _,book := range books{
+		fmt.Println("-", book.Title, "by", book.Author)
+	}
+}
+
+//stopping point - 3.4.1
