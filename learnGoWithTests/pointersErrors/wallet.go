432a84dd55c7b03c1f674553a883a865b49eb414
Sam Kikoma <kikomasamson@gmail.com>
2026-09-13 16:12:23 +0200

    Add Wallet implementation with Bitcoin type

---
+++ learnGoWithTests/pointersErrors/wallet.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+type Bitcoin int
+
+type Stringer interface {
+	String() string
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	if amount > w.balance {
+		return
+	}
+	w.balance -= amount
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
